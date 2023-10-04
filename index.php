d525b58b1825b3b02cf9111ff5f185122926fa8e
Gi Penayo <gigipenayo@gmail.com>
2023-10-04 12:32:33 -0300

    index

--- index.php
+++ index.php
@@ -131,7 +131,7 @@ $categorys=getCategory();
         <div class="footer-content">
             <div class="footer-links">
                 <a href="view/politic_private.php">Política de Privacidad</a>
-                <a href="#">Términos y Condiciones</a>
+                <a href="view/termin_condiction.php">Términos y Condiciones</a>
                 <a href="view/contact_page.php">Contacto</a>
             </div>
             <div class="footer-copyright">&copy; 2023 PlayTickets</div>  

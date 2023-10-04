099fbfb9aa8ec03116a9abd4b470630f043c0e62
Gi Penayo <gigipenayo@gmail.com>
2023-10-04 12:48:20 -0300

    contact funcional

--- view/contact_page.php
+++ view/contact_page.php
@@ -6,6 +6,9 @@
     <link rel="stylesheet" href="../assets/css/contact.css">
     <link rel="icon" type="image/png" href="../assets/img/logo.png.png">
     <link rel="stylesheet" href="../assets/css/barnavfooter.css">
+    <link rel="icon" type="img/logo" href="../assets/img/logo.png"> <!--!icono pestaña-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"><!--Icono de menu-->
+
 
     <title>Contacto</title>
 </head>
@@ -14,16 +17,17 @@
 
 <header>
         <div class="navbar">
-            <h1 class="logo"> 
-                <img src="../assets/img/logo.png.png" alt="Logo" height="80px ">PLAYTICKETS </h1>
-            <button class="accordion">Menú</button>
+            <img src="../assets/img/logo.png" alt="Logo" height="80px ">
+            <h1 class="logo"> PLAYTICKETS </h1>
+                
+            <button class="accordion"><i class="fas fa-bars"></i></button>
             <div class="panel">
 
                 <ul>
-                    <li><a href="index.php">Cartelera</a></li>
+                    <li><a href="../index.php">Cartelera</a></li>
                     <li><a href="../view/login.php">Ingresar</a></li>
                     <li><a href="../view/register.php">Registrarse</a></li>
-                    <li><a href="../view//contact_page.php">Contacto</a></li>
+                    
                 </ul>
             </div>
         </div>
@@ -32,12 +36,12 @@
     <div class="contact-container">
         <div class="company-info">
             <div class="company-logo">
-                <img src="../assets/img/logo.png.png" alt="Logo de la empresa">
+                <img src="../assets/img/logo.png" alt="Logo de la empresa">
             </div>
             <div class="company-details">
                 <h3>¡Nuestro Contacto!</h3>
-                <p>Dirección: Dirección de tu empresa</p>
-                <p>Teléfono: +123 456 789</p>
+                <p>Dirección: Colón 600, Merlo, Buenos Aires.</p>
+                <p>Teléfono: 0220-483-2403</p>
                 <p>Correo Electrónico: Playtikets1@gmail.com</p>
             </div>
         </div>
@@ -69,15 +73,21 @@
             </div>
         <div class="footer-content">
             <div class="footer-links">
-                <a href="#">Política de Privacidad</a>
-                <a href="#">Términos y Condiciones</a>
-                <a href="../view/contact_page.php">Contacto</a>
+            <a href="politic_private.php">Política de Privacidad</a>
+                <a href="termin_condiction.php">Términos y Condiciones</a>
+                <a href="contact_page.php">Contacto</a>
             </div>
             <div class="footer-copyright">
                 &copy; 2023 PlayTickets
             </div>
         </div>
     </footer>
-    <script src="../assets/js/barnavfooter.js"></script>
+    <script> document.addEventListener("DOMContentLoaded", function() {
+        const accordion = document.querySelector(".accordion");
+        const panel = document.querySelector(".panel");
+        accordion.addEventListener("click", function() {panel.style.display = panel.style.display === "block" ? "none" : "block";
+    });
+    });
+    </script>
 </body>
 </html>
